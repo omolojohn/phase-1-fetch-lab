fc8ac3b89bc1defbbc3fbad46237b4d191609d39
omolo-j <omoloj135@gmail.com>
2024-04-06 09:14:41 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(response => {
+            // Check if the response is successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            // Convert the response to JSON
+            return response.json();
+        })
+        .then(data => {
+            // Call renderBooks function and pass the JSON data
+            renderBooks(data);
+        })
+        .catch(error => {
+            console.error('There was a problem fetching the books:', error);
+        });
 }
 
 function renderBooks(books) {
